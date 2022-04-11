5ecd4012dcb9a090db90dce9ae73047afbc9ace1
Kim <narsha1001@gmail.com>
2022-04-11 16:21:19 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/jointree/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jointree.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp                            (
+      theme: ThemeData(
+
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  @override
+  void initState(){
+    super.initState();
+    if(Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   actions: <Widget>[
+      //     Navigatio
+      //   ],
+      // ),
+      body: SafeArea(
+        child: Builder(builder: (BuildContext context){
+          return WebView(
+            initialUrl: '',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController){
+              _controller.complete(webViewController);
+            },
+            onProgress: (int progress){},
+            javascriptChannels: <JavascriptChannel>{
+              _toasterJavascriptChannel(context)
+            },
+            gestureNavigationEnabled: true,
+          );
+        }),
+      ),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+}
